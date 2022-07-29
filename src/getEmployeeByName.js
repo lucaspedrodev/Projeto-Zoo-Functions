77f4fc4210476d22ff909bba0f728e462d1a0322
Lucas Pedro <rodrigerioo@gmail.com>
2022-07-29 16:48:46 -0300

    requisito 04

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const nomeSobrenome = employees.find(
+    (elemento) =>
+      employeeName === elemento.firstName || employeeName === elemento.lastName,
+  );
+  return nomeSobrenome;
 }
 
 module.exports = getEmployeeByName;
