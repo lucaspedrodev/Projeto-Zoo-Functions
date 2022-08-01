25a4c9df1aa0f7efc501378ac46a64e76bcc4f95
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-01 15:13:10 -0300

    exercicios 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,32 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para o argumento count deve retornar o número inteiro 4', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toEqual(expected);
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    const actual = handlerElephants('names');
+    const expected = [];
+    expect(actual).toEqual(expected);
+  });
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+  it('Para o argumento location deve retornar a string NW', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+});
