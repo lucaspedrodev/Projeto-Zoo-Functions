1d4497411f279a2578898e4dd22c4c35d78be250
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-02 14:23:22 -0300

    exercicio 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,23 @@
 // const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const countAnimals = (animal) => {
+  const semParamento = species.reduce((acc, valorCorrente) => {
+    acc[valorCorrente.name] = valorCorrente.residents.length;
+    return acc;
+  }, {});
+  if (animal === undefined) {
+    return semParamento;
+  }
+  if (animal.sex === undefined) {
+    const pegaSpecie = species.find((elemento) => elemento.name === animal.specie);
+    return pegaSpecie.residents.length;
+  }
+  const pegaSpecie = species.find((elemento) => elemento.name === animal.specie);
+  const mascFem = pegaSpecie.residents.filter((elemento) => elemento.sex === animal.sex);
+  return mascFem.length;
+};
 
-}
-
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 module.exports = countAnimals;
