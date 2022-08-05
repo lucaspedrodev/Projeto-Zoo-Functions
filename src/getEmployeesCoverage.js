2d5a07a50e606cc42d2bd904c8399b92c96ef2b1
Lucas Pedro <rodrigerioo@gmail.com>
2022-08-05 13:12:42 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,39 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function retornarDadosEspecie(listaEspecies) {
+  const nomesEspecies = [];
+  const locaisEspecies = [];
+  listaEspecies.forEach((idEspecie) => {
+    nomesEspecies.push(species.find((s) => s.id === idEspecie).name);
+    locaisEspecies.push(species.find((s) => s.id === idEspecie).location);
+  });
+  return { nomes: nomesEspecies, locais: locaisEspecies };
 }
 
+function retornarDadosColaborador(colaborador) {
+  const dadosEspecies = retornarDadosEspecie(colaborador.responsibleFor);
+  return {
+    id: colaborador.id,
+    fullName: `${colaborador.firstName} ${colaborador.lastName}`,
+    species: dadosEspecies.nomes,
+    locations: dadosEspecies.locais,
+  };
+}
+
+function getEmployeesCoverage(dados) {
+  if (dados != null) {
+    const colaborador = employees.find((e) => e.id === dados.id
+      || e.firstName === dados.name || e.lastName === dados.name);
+    if (colaborador == null) {
+      throw new Error('Informações inválidas');
+    }
+    return retornarDadosColaborador(colaborador);
+  }
+  const list = [];
+  employees.forEach((colaborador) => [
+    list.push(retornarDadosColaborador(colaborador)),
+  ]);
+  return list;
+}
 module.exports = getEmployeesCoverage;
